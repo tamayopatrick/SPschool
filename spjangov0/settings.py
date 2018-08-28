29a9f51cddce69c8be255144b59b2aacb5d09de1
tamayopatrick <tamayopatrick@gmail.com>
2018-08-28 13:59:03 +0300

    position03

--- spjangov0/settings.py
+++ spjangov0/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'landing.apps.LandingConfig',
-    'whitenoise.midleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
